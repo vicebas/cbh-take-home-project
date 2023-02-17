0d4dca7d110c223fdc2e345412c24abbe23163c9
Vinicius Gamaliel <viniciusgamaliel@MacBook-Pro-de-SytDigital.local>
2023-02-17 13:18:23 -0300

    refactoring dpk

--- dpk.js
+++ dpk.js
@@ -1,28 +1,30 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+const deterministicPartitionKey = (event) => {
+
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY
   }
+  
+  let candidate = event.partitionKey || crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
+  
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
+  } 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
+  
   return candidate;
 };
+
+
+module.exports = {
+  TRIVIAL_PARTITION_KEY,
+  MAX_PARTITION_KEY_LENGTH,
+  deterministicPartitionKey
+}
